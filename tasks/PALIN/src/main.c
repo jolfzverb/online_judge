30236f3edd020cf18aa4befd0d4fa9e4f8efebb3
Yury Usishchev <y.usishchev@samsung.com>
2013-12-25 13:37:32 +0400

    omw to rewrite PALIN

--- tasks/PALIN/src/main.c
+++ tasks/PALIN/src/main.c
@@ -2,18 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+struct num{
+  char * start;
+  char * compare_left;
+  char * compare_right;
+  char * increment_start;
+  char * copy_left;
+  char * copy_right;
+};
+increment_and_dublicate(char * num){
+
+}
+
+dublicate(char * num){
+
+}
+
+void maybe_increment(char*num){
+
+
+}
 // TODO: 9999
 int main(){
   int N;
   int i;
   int k;
   scanf("%d", &N);
-  char * number = malloc(1000000*sizeof(char));
+  char * number = malloc(1000004*sizeof(char));
   for(i = 0; i < N; i++){
 
     scanf("%s", number);  // > 0
+    maybe_increment(number);
 
-    int len = strlen(number);
+    /*int len = strlen(number);
 
     if (len%2) {
       if(number[len / 2 - 1] <= number[len / 2 + 1]){
@@ -32,8 +54,10 @@ int main(){
         printf("%c", number[k]);
       for(k = len/2-1; k>=0; k--)
         printf("%c", number[k]);
+
     } 
-    printf("\n");
+    */
+    printf("%s\n", number);
   }
   return 0;
 }
