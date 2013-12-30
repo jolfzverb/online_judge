c52eecfc874370f9ecc6ab81c7b24e4a0889c119
Yury Usishchev <y.usishchev@samsung.com>
2013-12-30 19:45:29 +0400

    new PALIN algorithm, accepted on spoj

--- tasks/PALIN/src/main.c
+++ tasks/PALIN/src/main.c
@@ -8,18 +8,78 @@ struct num{
   char * compare_left;
   char * compare_right;
   char * increment_start;
-  char * copy_left;
-  char * copy_right;
 };
-increment_and_dublicate(char * num){
 
+
+int all_nines(struct num a){
+  char * ololo = a.start;
+  while(*ololo)
+    if(*ololo++ != '9')
+      return 0;
+  return 1;
 }
 
-dublicate(char * num){
 
+void handle_nines(struct num a){
+  int len = strlen(a.start);
+  printf("1");
+  while(--len)
+    printf("0");
+  printf("1\n");
 }
 
-void maybe_increment(char*num){
+void indexer(struct num * a, char * num){
+  int len = strlen(num);
+  a->start = num;
+  if(len % 2) {
+    a->compare_left = num + len/2 - 1;
+    a->compare_right = num + len/2 + 1;
+  } else {
+    a->compare_left = num + len/2 - 1;
+    a->compare_right = num + len/2;
+  }
+  a->increment_start = num + (len - 1) / 2;
+
+}
+int increment_needed(struct num a){
+  char * left = a.compare_left;
+  char * right = a.compare_right;
+  while(*right) {
+    if (*left != *right)
+      return *left > *right ? 0 : 1;
+    left --;
+    right ++;
+  }
+  return 1;
+}
+void increment(struct num a){
+  char *cur = a.increment_start;
+  while(*cur == '9')
+    *cur-- = '0';
+  (*cur)++;
+}
+void duplicate(struct num a){
+  char * left = a.compare_left;
+  char * right = a.compare_right;
+  while(*right) {
+    *right++ = *left--;
+  }
+}
+void print_num(struct num a){
+  printf("%s\n",a.start);
+}
+
+void process(char*num){
+  struct num a;
+  indexer(&a, num);
+  if(all_nines(a))
+    return handle_nines(a);
+    
+  if(increment_needed(a))
+    increment(a);
+
+  duplicate(a);
+  print_num(a);
 
 
 }
@@ -33,7 +93,7 @@ int main(){
   for(i = 0; i < N; i++){
 
     scanf("%s", number);  // > 0
-    maybe_increment(number);
+    process(number);
 
     /*int len = strlen(number);
 
@@ -57,7 +117,6 @@ int main(){
 
     } 
     */
-    printf("%s\n", number);
   }
   return 0;
 }
