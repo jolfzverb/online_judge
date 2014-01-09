ba73731652a311c540fee92a5c60d0c793c544a6
Yury Usishchev <y.usishchev@samsung.com>
2014-01-09 18:49:32 +0400

    MAXWOODS task added, TODO: find bug in it

---
+++ tasks/MAXWOODS/src/main.c
@@ -0,0 +1,77 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(){
+  int N;
+  int * layout;
+  int * sums;
+  layout = malloc(200*sizeof(int));//i dont think we need this either
+  sums = malloc(200*sizeof(int));//we need only one string
+  char * str = malloc(200*sizeof(char));
+  scanf("%d",&N);
+  int i,j,k;
+  int m,n;
+  int max;
+  for (i = 0; i < N; i++){
+    for (j = 0; j < 200; j ++)
+      sums[j] = -1;
+    sums[0] = 0;
+    scanf("%d",&m);
+    scanf("%d",&n);
+    max = 0;
+    for(k = 0; k < n; k++){
+        char a;
+      //read one line;
+        scanf("%s", str);
+      for(j = 0; j < m; j++){
+        switch (str[j]){
+          case '0':
+            layout[j] = 0;
+            break;
+          case 'T':
+            layout[j] = 1;
+            break;
+          case '#':
+            layout[j] = -1;
+            break;
+          default:
+            break;
+        }
+      }
+      int start;
+      int step;
+      int end;
+      if(k%2){
+        start = m-1;
+        step = -1;
+        end = -1;
+      } else {
+        start = 0;
+        step = 1;
+        end = m;
+      }
+      int down;
+      int side;
+      for(j = start; j != end; j += step){
+        if(j == start){
+          if(layout[j] == -1 || sums[j] == -1){
+            sums[j] = -1;
+          } else {
+            sums[j] += layout[j];
+          }
+        } else {
+          if(layout[j] == -1){
+            sums[j] = -1;
+          } else {
+            down = sums[j] == -1 ? -1 : sums[j] + layout[j];
+            side = sums[j-step] == -1 ? -1 : sums[j-step] + layout[j];
+            sums[j] = down > side ? down : side;
+          }
+        }
+        max = sums[j]>max?sums[j]:max;
+      }
+    }
+    printf("%d\n", max);
+  }
+  return 0;
+}
