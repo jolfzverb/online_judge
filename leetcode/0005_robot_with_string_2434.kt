6a74191337bb97bb0b7f66742ce10c8d607a3a12
Yury Usishchev <jolfzverb@gmail.com>
2024-07-13 22:51:32 +0200

    Add kotlin solution to 2434

---
+++ leetcode/0005_robot_with_string_2434.kt
@@ -0,0 +1,34 @@
+/* https://leetcode.com/problems/using-a-robot-to-print-the-lexicographically-smallest-string/description/
+ */
+class Solution {
+  fun robotWithString(str: String): String {
+    val s = str.toCharArray()
+    val indexes = Array<Int>(s.size) { 0 }
+    var min_index = s.size - 1
+
+    for (i in s.size - 1 downTo 0) {
+      if (s[i] < s[min_index]) {
+        min_index = i
+      }
+
+      indexes[i] = min_index
+    }
+
+    var result = ArrayList<Char>()
+    val stack = ArrayDeque<Char>()
+
+    for ((i, c) in s.withIndex()) {
+      while (!stack.isEmpty() && stack.last() <= s[indexes[i]]) {
+        result.add(stack.removeLast())
+      }
+
+      stack.addLast(s[i])
+    }
+
+    while (!stack.isEmpty()) {
+      result.add(stack.removeLast())
+    }
+
+    return result.joinToString("")
+  }
+}
