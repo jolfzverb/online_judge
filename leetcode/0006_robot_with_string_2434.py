7aedae3ad5157731d8e56a0b4b4744bfe6a9ba5f
Yury Usishchev <jolfzverb@gmail.com>
2024-07-13 22:51:32 +0200

    Add python solution to 2434

---
+++ leetcode/0006_robot_with_string_2434.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/using-a-robot-to-print-the-lexicographically-smallest-string/
+
+class Solution(object):
+    def robotWithString(self, s):
+        smallest_index = len(s) - 1
+        indexes = [0 for i in range(len(s))]
+
+        for j in range(len(s), 0, -1):
+            i = j-1
+            if s[i] < s[smallest_index]:
+                smallest_index = i
+
+            indexes[i] = smallest_index
+
+        stack = []
+        result = []
+
+        for i, c in enumerate(s):
+            while len(stack) > 0 and stack[-1] <= s[indexes[i]]:
+                result.append(stack.pop())
+
+            stack.append(c)
+
+        while len(stack) > 0:
+            result.append(stack.pop())
+
+        return ''.join(result)
