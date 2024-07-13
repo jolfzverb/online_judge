9b2c7446b7cda1bd06abfe437487b08b3ad2844f
Yury Usishchev <jolfzverb@gmail.com>
2024-07-13 22:51:32 +0200

    Add second solution to 2434

---
+++ leetcode/cpp/1_robot_with_string_2434_2.cpp
@@ -0,0 +1,47 @@
+/* https://leetcode.com/problems/using-a-robot-to-print-the-lexicographically-smallest-string/description/
+ */
+
+class Solution {
+ public:
+  string robotWithString(string s) {
+    if (s.empty()) return s;
+
+    std::vector<size_t> smallest_index;
+    smallest_index.resize(s.size());
+
+    size_t last_smallest = s.size() - 1;
+
+    for (size_t j = s.size(); j > 0; --j) {
+      size_t i = j - 1;
+
+      if (s[i] < s[last_smallest]) {
+        smallest_index[i] = i;
+        last_smallest = i;
+      } else {
+        smallest_index[i] = last_smallest;
+      }
+    }
+
+    std::string result;
+    std::stack<char> stack;
+
+    for (size_t i = 0; i < s.size(); i++) {
+      while (!stack.empty() && stack.top() <= s[smallest_index[i]]) {
+        result += stack.top();
+        stack.pop();
+      }
+      if (i != smallest_index[i]) {
+        stack.push(s[i]);
+      } else {
+        result += s[i];
+      }
+    }
+
+    while (!stack.empty()) {
+      result += stack.top();
+      stack.pop();
+    }
+
+    return result;
+  }
+};
